63cefa58d475c96ff0001661bc373df0116f47c8
BoLin Chen <s0932247522@gmail.com>
2021-11-15 11:44:30 +0800

    20211115-test9

--- app.py
+++ app.py
@@ -147,9 +147,9 @@ def handle_message(event):
         sh = gc.open_by_url(gs_url)
         ws = sh.worksheet_by_title('now')
         tt = ws.get_values('A:A','G:G')
-        val = '隊員\t 個人\t 團隊\n'
+        val = '隊員\t\t\t 個人\t 團隊\n'
         for cel in range(5,len(tt)):
-            val +=  tt[cel][4] + '\t' + tt[cel][5] + '\t' + tt[cel][6] + '\n' 
+            val +=  tt[cel][4] + '\t\t\t' + tt[cel][5] + '\t' + tt[cel][6] + '\n' 
 
 
 
